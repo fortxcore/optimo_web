05e7a1ae685152eb1e54ce5d2efa357c97e133bf
Ravindu Kavishka <ravindukd@gmail.com>
2021-09-11 16:14:29 +0530

    Removed a Comment

--- src/routes/index.js
+++ src/routes/index.js
@@ -16,16 +16,6 @@ const Refferals = lazy(() => import('../pages/refferal/refferals'))
 const Inbox = lazy(() => import('../pages/refferal/messages'))
 const ProfileSetup = lazy(() => import('../pages/auth/profile-info'))
 
-/**
- * ⚠ These are internal routes!
- * They will be rendered inside the app, using the default `containers/Layout`.
- * If you want to add a route to, let's say, a landing page, you should add
- * it to the `App`'s router, exactly like `Login`, `CreateAccount` and other pages
- * are routed.
- *
- * If you're looking for the links rendered in the SidebarContent, go to
- * `routes/sidebar.js`
- */
 const routes = [
   {
     path: '/dashboard', // the url
