57b8f8b428d2c46bec10f65d570575d18a99c19e
Ravindu Kavishka <ravindukd@gmail.com>
2021-09-11 16:03:55 +0530

    Route fixes

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,12 +2,6 @@ import { lazy } from 'react'
 
 // use lazy for better code splitting, a.k.a. load faster
 const Dashboard = lazy(() => import('../pages/Dashboard'))
-const Forms = lazy(() => import('../pages/Forms'))
-const Cards = lazy(() => import('../pages/Cards'))
-const Charts = lazy(() => import('../pages/Charts'))
-const Buttons = lazy(() => import('../pages/Buttons'))
-const Modals = lazy(() => import('../pages/Modals'))
-const Tables = lazy(() => import('../pages/Tables'))
 const Page404 = lazy(() => import('../pages/404'))
 const Blank = lazy(() => import('../pages/Blank'))
 
@@ -56,10 +50,6 @@ const routes = [
   {
     path: '/inbox',
     component: Inbox,
-  },
-  {
-    path: '/modals',
-    component: Modals,
   },
   {
     path: '/deposits',
